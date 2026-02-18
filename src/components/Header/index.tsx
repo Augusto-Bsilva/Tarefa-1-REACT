b8393250ca6f317b19e04eaeda9e973652f0adab
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-18 10:30:47 -0300

    feat: adicionando componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import style from "./styles.module.css"
+import Logo from "../assets/Logo.svg"
+
+
+export default function Header(){
+    return(
+        <header className={style.header}>
+            <img src={Logo}></img>
+        </header>
+    )
+}

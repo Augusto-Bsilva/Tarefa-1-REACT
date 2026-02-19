44c32925934e97cfea2b9fc6e8b47bf5964395a7
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-19 09:42:05 -0300

    feat: implementando mecanismo do meu comentário

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,9 +1,10 @@
 import style from './styles.module.css'
-
+import Post from '../Post'
 export default function Feed(){
     return(
      <div className={style.feed}>
-
+            <Post autorPic="https://as2.ftcdn.net/jpg/01/25/68/37/1000_F_125683702_KzYgSIAUqhI6wmxNL49pgG6XafE3E5pO.jpg" name="Ruan" cargo="Dev Back-end" />
+             <Post autorPic="https://as2.ftcdn.net/jpg/01/25/68/37/1000_F_125683702_KzYgSIAUqhI6wmxNL49pgG6XafE3E5pO.jpg" name="Ruan" cargo="Dev Back-end" />
      </div>
     )
 }
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,28 +1,67 @@
 import style from './styles.module.css'
 import type { PostProps } from '../../Types/Post'
+import { useState } from 'react'
+import MyComent from '../Coment'
+import MyPic from '../assets/IMG_4150.jpeg'
 
-export default function Post({ autorPic,name,cargo }:PostProps){
+export default function Post({  autorPic, name, cargo }:PostProps){
+    const [coments, setComents] = useState<string[]>([])
+    const [coment,setComent] = useState('')
+
+     function handleSubmit(e){
+        e.preventDefault();
+        setComents([...coments, coment]);
+        setComent('')
+    }
+
+     function handleChange(e){
+        setComent(e.target.value);
+     }
+    
     return(
-     <div className={style.pub}>
-        <header>
-            <div>
-                <img
+     <aside className={style.pub}>
+        <header className={style.profilePostInfo}>
+            <div className = {style.profileData}>
+                <img 
+                className={style.profileImage}
                 src={autorPic}/>
-                <div>
+                <div className={style.profileDataText}>
                     <h3>{name}</h3>
                     <p>{cargo}</p>
                 </div>
             </div>
-            <p>Publicado há 1h</p>
+            <p className={style.postTime}>Publicado há 1h</p>
         </header>
-        <section>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quidem molestiae optio, repellat unde incidunt odit modi voluptatem odio sit temporibus enim dolorum earum saepe dicta quae libero inventore exercitationem!</p>
+        <section className={style.postText}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quidem molestiae optio, repellat unde incidunt odit modi voluptatem odio sit temporibus enim dolorum earum saepe dicta quae libero inventore exercitationem!</p>
         </section>
-        <form>
-            <h2>Deixe seu feedbakc</h2>
-            <textarea />
-            <button type='submit'>Publicar</button>
+        <form onSubmit={handleSubmit} className={style.feedback}>
+            
+            <h2>Deixe seu feedback</h2>
+            
+            <textarea 
+            className={style.input} 
+            placeholder='Escreva um comentário...' 
+            value={coment}
+            onChange = {handleChange}
+            />
+            
+            <button type='submit'>Comentar</button>
+
         </form>
-     </div>
+        <div>
+            {coments.map((coment,index) => {
+                return (
+                    <MyComent 
+                        key = {index}
+                        text= {coment}
+                        name = 'Augusto'
+                        autorPic={MyPic}
+                    />
+                )
+            }
+            )}
+        </div>
+     </aside>
     )
 }
